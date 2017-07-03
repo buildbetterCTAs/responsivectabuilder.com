82935026bce6cee04919bd9217d4be00b752284c
Jeff Reiner <theundeadghost@gmail.com>
2017-07-03 16:40:32 -0400

    :sparkles: More work for #3
     #3

--- src/App.vue
+++ src/App.vue
@@ -11,8 +11,8 @@
         </div>
         <div class="nav-right">
           <a href="#features" class="nav-item is-hidden-mobile">Features</a>
-          <a class="nav-item is-hidden-mobile">Documentation</a>
-          <a class="nav-item is-hidden-mobile">Blog</a>
+          <a class="nav-item is-hidden-mobile" href="https://docs.responsivectabuilder.com" target="_blank">Documentation</a>
+          <a class="nav-item is-hidden-mobile" href="https://blog.responsivectabuilder.com" target="_blank">Blog</a>
           <div class="nav-item is-hidden-mobile">
             <div class="field is-grouped">
               <p class="control">
@@ -36,23 +36,25 @@
     </nav>
 
     <!-- BODY CONTENT -->
-    <section class="hero is-medium">
+    <section class="hero">
       <div class="hero-body">
         <div class="container has-text-centered">
           <h1 class="title is-spaced is-2 dodgerText">Build better calls-to-action</h1>
           <h2 class="subtitle">Responsive CTA Builder helps marketers build responsive calls-to-action which will convert more traffic into visitors than your current bitmap image call-to-action. Give it a try, for free.</h2>
         </div>
         <div class="container">
-          <!-- CTA -->
-          <div class="cta" style="border-radius: 4px; background-color: #0E589A;">
-            <div class="ctaHeadline" style="color: #ffffff;">This is a powerful, eye-catching headline</div>
-            <div class="ctaDescription" style="color: #ffffff;">This is your secondary text that might explain why your reader should follow your call-to-action.</div>
-            <a class="ctaButton" target="_blank" style="cursor: initial; background-color: #48A7F9; color: #ffffff;">click here, reader!</a>
+          <div class="is-cta-container">
+            <!-- CTA -->
+            <div class="cta" style="border-radius: 4px; background-color: #0E589A;">
+              <div class="ctaHeadline" style="color: #ffffff;">This is a powerful, eye-catching headline</div>
+              <div class="ctaDescription" style="color: #ffffff;">This is your secondary text that might explain why your reader should follow your call-to-action.</div>
+              <a class="ctaButton" target="_blank" style="cursor: initial; background-color: #48A7F9; color: #ffffff;">click here, reader!</a>
+            </div>
+            <!-- /CTA -->
           </div>
-          <!-- /CTA -->
         </div>
         <div class="container has-text-centered">
-          <a href="https://app.responsivectabuilder.com" class="button is-stripe is-primary is-medium">Try Building Your Own</a>
+          <a href="https://app.responsivectabuilder.com" class="button is-attention is-danger is-medium">Try Building Your Own</a>
         </div>
       </div>
     </section>
@@ -67,78 +69,83 @@
     </section>
 
     <!-- FEATURES -->
-    <section id="features" class="hero is-medium">
-      <div class="hero-body">
+    <section id="features">
+      <div class="features">
+        <!-- ONE -->
+        <div class="featureBlock block1">
+          <div class="container">
+            <div class="columns wow slideInLeft">
+              <div class="column">
+                <img src="./assets/devices.svg">
+              </div>
+              <div class="column is-narrow">
 
-        <div class="container features">
-          <!-- ONE -->
-          <div class="columns wow slideInLeft">
-            <div class="column">
-              <img src="./assets/devices.svg">
-            </div>
-            <div class="column">
-              <p>Responsive CTAs make your call-to-action reader friendly on all devices: computers, tablets and mobile phones.</p>
-            </div>
-          </div>
-          <!-- TWO -->
-          <div class="columns wow slideInRight">
-            <div class="column">
-              <p>Responsive CTAs are proven to perform better than image CTAs. We’ve seen <strong>2.5x more clicks</strong> on Responsive CTAs than Image CTAs.</p>
-            </div>
-            <div class="column">
-              <img src="./assets/clicks.svg">
-            </div>
-          </div>
-          <!-- THREE -->
-          <div class="columns wow slideInLeft">
-            <div class="column">
-              <img src="./assets/dashboard.svg">
-            </div>
-            <div class="column">
-              <p>You can link your HubSpot CTA tracking with our Responsive CTA Builder. Build beautiful Responsive CTAs but don’t lose any tracking or reporting capabilities.</p>
-            </div>
-          </div>
-          <!-- FOUR -->
-          <div class="columns wow slideInRight">
-            <div class="column">
-              <p>Forget trying to fit your long tail keywords into your alt text for an image CTA. With Responsive CTAs just add your keywords into the header, secondary text or button text.</p>
-            </div>
-            <div class="column">
-              <img src="./assets/keyword.svg">
+              </div>
+              <div class="column">
+                <p>Responsive CTAs make your call-to-action reader friendly on all devices: computers, tablets and mobile phones.</p>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-
-    <!-- REQUEST BETA -->
-    <section class="hero is-primary is-medium is-bold">
-      <div class="hero-body has-text-centered">
-        <div class="container">
-          <h1 class="title is-spaced">Coming Soon</h1>
-          <h2 class="subtitle">Request Beta Access</h2>
-          <!-- BEGIN MAILCHIMP -->
-          <div id="mc_embed_signup">
-            <form action="//responsivectabuilder.us15.list-manage.com/subscribe/post?u=5dc16787f107b34340beed700&amp;id=759e564468" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-              <div id="mc_embed_signup_scroll" class="field has-addons has-addons-centered">
-                <div class="mc-field-group control">
-                  <input type="email" placeholder="Email" value="" name="EMAIL" class="input required email" id="mce-EMAIL">
-                </div>
-                <div id="mce-responses" class="clear">
-                  <div class="response" id="mce-error-response" style="display:none"></div>
-                  <div class="response" id="mce-success-response" style="display:none"></div>
-                </div>
-                <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                  <input type="text" name="b_5dc16787f107b34340beed700_759e564468" tabindex="-1" value="">
-                </div>
-                <div class="clear control">
-                  <input type="submit" value="Submit" name="subscribe" id="mc-embedded-subscribe" class="button">
-                </div>
+        <!-- TWO -->
+        <div class="featureBlock block2">
+          <div class="container">
+            <div class="columns wow slideInRight">
+              <div class="column">
+                <p>Responsive CTAs are proven to perform better than image CTAs. We’ve seen <strong>2.5x more clicks</strong> on Responsive CTAs than Image CTAs.</p>
               </div>
-            </form>
+              <div class="column is-narrow">
+
+              </div>
+              <div class="column">
+                <img src="./assets/clicks.svg">
+              </div>
+            </div>
           </div>
-          <!-- END MAILCHIMP -->
+        </div>
+        <!-- THREE -->
+        <div class="featureBlock block3">
+          <div class="container">
+            <div class="columns wow slideInLeft">
+              <div class="column">
+                <img src="./assets/dashboard.svg">
+              </div>
+              <div class="column is-narrow">
+
+              </div>
+              <div class="column">
+                <p>You can link your HubSpot CTA tracking with our Responsive CTA Builder. Build beautiful Responsive CTAs but don’t lose any tracking or reporting capabilities.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- FOUR -->
+        <div class="featureBlock block4">
+          <div class="container">
+            <div class="columns wow slideInRight">
+              <div class="column">
+                <p>Forget trying to fit your long tail keywords into your alt text for an image CTA. With Responsive CTAs just add your keywords into the header, secondary text or button text.</p>
+              </div>
+              <div class="column is-narrow">
+
+              </div>
+              <div class="column">
+                <img src="./assets/keyword.svg">
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- CTA -->
+        <div class="featureBlock block3">
+          <div class="container">
+            <!-- CTA -->
+            <div class="cta" style="border-radius: 4px; background-color: #0E589A;">
+              <div class="ctaHeadline" style="color: #ffffff;">This is a powerful, eye-catching headline</div>
+              <div class="ctaDescription" style="color: #ffffff;">This is your secondary text that might explain why your reader should follow your call-to-action.</div>
+              <a class="ctaButton" target="_blank" style="cursor: initial; background-color: #48A7F9; color: #ffffff;">click here, reader!</a>
+            </div>
+            <!-- /CTA -->
+          </div>
         </div>
       </div>
     </section>
@@ -146,9 +153,16 @@
     <!-- FOOTER -->
     <footer class="footer">
       <div class="container">
-        <div class="content has-text-centered">
-          <p>Made with 💙 by <a href="https://www.reiner.io">Jeff Reiner</a> and <a href="https://twitter.com/_danieljmurphy">Daniel Murphy</a>.</p>
-          <p><small>© 2017 Daniel J. Murphy and Jeff Reiner</small></p>
+        <div class="has-text-centered">
+          <img src="./assets/wordmark.svg" alt="Wordmark">
+          <div class="nav-center links">
+            <a href="#contact" class="nav-item">Contact</a>
+            <a href="https://docs.responsivectabuilder.com" target="_blank" class="nav-item">Documentation</a>
+            <a href="https://blog.responsivectabuilder.com" target="_blank" class="nav-item">Blog</a>
+            <a href="#story" class="nav-item">Story</a>
+          </div>
+          <!-- <p>Made with 💙 by <a href="https://www.reiner.io">Jeff Reiner</a> and <a href="https://twitter.com/_danieljmurphy">Daniel Murphy</a>.</p> -->
+          <p class="copyright"><small>© 2017 Daniel J. Murphy and Jeff Reiner</small></p>
           <div class="nav-center">
             <a class="nav-item" href="https://twitter.com/buildBetterCTAs" target="_blank">
               <span class="icon">
@@ -158,6 +172,11 @@
             <a class="nav-item" href="https://www.facebook.com/BuildBetterCTAs" target="_blank">
               <span class="icon">
                 <i class="fa fa-facebook"></i>
+              </span>
+            </a>
+            <a class="nav-item" href="#linkedin" target="_blank">
+              <span class="icon">
+                <i class="fa fa-linkedin"></i>
               </span>
             </a>
           </div>
@@ -219,9 +238,14 @@ $white: #fff
 $black: #000
 $grey50: #fafafa
 $grey100: #f5f5f5
+$grey200: #eee
 $grey300: #e0e0e0
 $grey400: #bdbdbd
 $grey500: #9e9e9e
+$grey600: #757575
+$grey700: #616161
+$grey800: #424242
+$grey900: #212121
 
 $primary: $dodger
 
@@ -250,22 +274,40 @@ $radius: 4px // sass-lint:disable-line variable-name-format
 .subtitle
   color: $text
 
-.hero-body // sass-lint:disable-line class-name-format
-  .container
-    &:not(:first-child)
-      margin-top: 64px
+// .hero-body // sass-lint:disable-line class-name-format
+//   .container
+//     &:not(:first-child)
+//       margin-top: 64px
 
 .nav
   background-color: transparent
 
+.is-cta-container
+  margin: 64px 0
+
 .features
+  .featureBlock
+    padding: 80px 0
+
+    &.block1
+      background-color: darken($white, 0%)
+
+    &.block2
+      background-color: darken($white, 2.5%)
+
+    &.block3
+      background-color: darken($white, 5%)
+
+    &.block4
+      background-color: darken($white, 7.5%)
+
   .columns
     align-items: center
 
-
-.is-stripe // sass-lint:disable-line class-name-format
+.is-attention // sass-lint:disable-line class-name-format
   // sass-lint:disable-block no-color-literals
   box-shadow: 0 4px 6px rgba(50, 50, 93, .11),  0 1px 3px rgba(0, 0, 0, .08)
+  transition: box-shadow .2s ease, background-color .2s ease
 
   &:hover
     box-shadow: 0 7px 14px rgba(50, 50, 93, .1),  0 3px 6px rgba(0, 0, 0, .08)
@@ -280,8 +322,15 @@ html
 .white
   background-color: $white !important
 
-.footer,
-.grey50
-  background-color: $grey50
+.footer
+  background-color: $white
+  padding: 5rem
+
+  .links
+    margin-top: 24px
+
+  .copyright
+    margin-bottom: 32px
+    margin-top: 16px
 
 </style>
